d91cd45acf746bdb1f0f75364fb8a2b7c54a268b
Yusuf Suhair <yusufmohdsuhair@gmail.com>
2021-08-09 17:03:40 +0800

    13 - While Loop

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,19 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        int hari = 10;
-        String jawapan;
 
-        switch (hari) {
-            case 5:
-                jawapan = "Hari ini adalah hari Jumaat";
-                break;
-            case 6:
-                jawapan = "Hari ini adalah hari Sabtu";
-                break;
-            case 7:
-                jawapan = "Hari ini adalah hari Ahad";
-                break;
-            default:
-                jawapan = "Hari ini adalah saya pun tidak tahu";
+        // While
+        int i = 0;
+
+//        while (i < 5) {
+//            System.out.println(i++);
+//        }
+
+        // Do-While
+        do {
+            System.out.println(i++);
         }
 
-        System.out.println(jawapan);
+        while (i < 5);
     }
 }
