22360996c27b0224740d516c34911f60970851c0
Yusuf Suhair <yusufmohdsuhair@gmail.com>
2021-08-09 16:54:14 +0800

    12 - Switch

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,23 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        int hari = 10;
         String jawapan;
-        int umur = 10;
 
-        if (umur == 10) {
-             jawapan = "Awak adalah sepuluh tahun";
-        } else if (umur == 12) {
-            jawapan = "Awak adalah dua belas tahun";
-        } else if (umur == 14) {
-            jawapan = "Awak adalah empat belas tahun";
-        } else {
-            jawapan = "Saya tidak tahu umur awak";
+        switch (hari) {
+            case 5:
+                jawapan = "Hari ini adalah hari Jumaat";
+                break;
+            case 6:
+                jawapan = "Hari ini adalah hari Sabtu";
+                break;
+            case 7:
+                jawapan = "Hari ini adalah hari Ahad";
+                break;
+            default:
+                jawapan = "Hari ini adalah saya pun tidak tahu";
         }
 
         System.out.println(jawapan);
-
-        // Ternary
-        jawapan = 5 < 3 ? "Betul, lima adalah lebih daripada tiga" : "Salah, lima adalah lebih daripada tiga";
-        System.out.println("Jawapan daripada Ternary: " + jawapan);
     }
 }
