2ed92ffcae1091e17f29fae5de8266bcbe7da272
Yusuf Suhair <yusufmohdsuhair@gmail.com>
2021-08-10 16:40:50 +0800

    15 - Break Continue

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        // Break
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
 
+        // Continue
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
