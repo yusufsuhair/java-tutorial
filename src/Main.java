a8023a59f2f4961a673047173c84aa305ad3a6f6
Yusuf Suhair <yusufmohdsuhair@gmail.com>
2021-08-10 16:20:30 +0800

    14 - For Loop

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
-import java.util.Locale;
-
 public class Main {
     public static void main(String[] args) {
 
-        // While
-        int i = 0;
-
-//        while (i < 5) {
-//            System.out.println(i++);
-//        }
-
-        // Do-While
-        do {
-            System.out.println(i++);
-        }
-
-        while (i < 5);
     }
 }
