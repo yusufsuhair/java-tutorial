b770be42437d73a0561e7d0564239fce80f323a4
Yusuf Suhair <yusufmohdsuhair@gmail.com>
2021-08-15 19:00:06 +0800

    20 - Methods Overloading

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        // Break
-        for (int i = 0; i < 10; i++) {
-            if (i == 4) {
-                break;
-            }
-            System.out.println(i);
-        }
 
-        // Continue
-        for (int i = 0; i < 10; i++) {
-            if (i == 4) {
-                continue;
-            }
-            System.out.println(i);
-        }
+        // Method Overloading
+        System.out.println(perkenalkanDiri("Amin", 10)); // Integer);
+        System.out.println(perkenalkanDiri("Asyraf", 10.0)); // Double);
+        System.out.println(perkenalkanDiri("Aziz", 9.0f)); // Float);
+    }
+
+    static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    static int tambah(int a, double b) {
+        return (int) (a + b);
+    }
+
+    static String perkenalkanDiri(String nama, int umur) {
+        return "Nama saya adalah " + nama + " dan umur saya adalah " + umur;
+    }
+
+    static String perkenalkanDiri(String nama, double umur) {
+        return "Nama saya adalah " + nama + " dan umur saya adalah " + umur;
+    }
+
+    static String perkenalkanDiri(String nama, float umur) {
+        return "Nama saya adalah " + nama + " dan umur saya adalah " + umur;
     }
 }
